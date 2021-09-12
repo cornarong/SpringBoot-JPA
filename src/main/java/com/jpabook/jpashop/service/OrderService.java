27aa00473a8738995843797285eac09882731142
cornarong <cornarong@gmail.com>
2021-09-12 20:14:49 +0900

    주문 서비스 개발중2

--- src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -1,9 +1,6 @@
 package com.jpabook.jpashop.service;
 
-import com.jpabook.jpashop.domain.Delivery;
-import com.jpabook.jpashop.domain.DeliveryStatus;
-import com.jpabook.jpashop.domain.Member;
-import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.*;
 import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.repository.ItemRepository;
 import com.jpabook.jpashop.repository.MemberRepository;
@@ -29,20 +26,33 @@ public class OrderService {
         // 엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
-
         // 배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
-
         // 주문상품 생성
-//        OrderItem.createOrderItem()
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+        // 주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
     }
 
-    // 취소
+    /**
+     * 주문 취소
+     */
+    @Transactional(readOnly = false)
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
 
-    // 검색
-
-
-
+    // 검색 (차후 개발, 잠시 보류)
+    /*public List<Order> findOrder(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }*/
 
 }
